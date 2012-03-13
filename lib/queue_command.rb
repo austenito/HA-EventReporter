ebfc961fbe72ac20e29d78456cd369d097757d47
Austen Ito <austen.services@gmail.com>
2012-03-13 15:01:15 -0400

    Queue regex

--- lib/queue_command.rb
+++ lib/queue_command.rb
@@ -1,6 +1,8 @@
 require "printer"
 
 class QueueCommand
+  VALID_ATTR = "regdate|first_name|last_name|email_address|homephone|" +
+                "street|city|state|zipcode"
   attr_reader :queue, :printer
 
   def initialize(queue = Queue.new, printer = Printer.new)
@@ -18,5 +20,9 @@ class QueueCommand
       attribute = commands[2]
       printer.send("#{action}_#{action_modifier}", attribute)
     end
+  end
+
+  def is_valid_query?(query)
+    (query =~ /print( by (#{VALID_ATTR}))?$/) == 0
   end
 end
--- spec/queue_command_spec.rb
+++ spec/queue_command_spec.rb
@@ -3,7 +3,7 @@ require 'queue'
 require 'queue_command'
 require 'printer'
 
-describe QueueCommand do
+describe QueueCommand, "(print|print by|save to)" do
   before(:each) do
     @queue = mock(Queue)
     @printer = mock(Printer)
@@ -26,5 +26,23 @@ describe QueueCommand do
     @queue.stub(:responds_to?).with("save").and_return(false)
     @printer.should_receive(:save_to).with("testfile")
     @command.run("save to testfile")
+  end
+
+  it "accepts supported print attributes" do
+    @command.is_valid_query?("print").should == true
+    @command.is_valid_query?("print by regdate").should == true
+    @command.is_valid_query?("print by first_name").should == true
+    @command.is_valid_query?("print by last_name").should == true
+    @command.is_valid_query?("print by homephone").should == true
+    @command.is_valid_query?("print by email_address").should == true
+    @command.is_valid_query?("print by street").should == true
+    @command.is_valid_query?("print by state").should == true
+    @command.is_valid_query?("print by city").should == true
+    @command.is_valid_query?("print by zipcode").should == true
+  end
+
+  it "does not accept unsupported print attributes" do
+    @command.is_valid_query?("prints hi").should == false
+    @command.is_valid_query?("print hi").should == false
   end
 end
