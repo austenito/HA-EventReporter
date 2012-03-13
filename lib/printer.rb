d06bfef4d3d987d07d0452a32fb152cdd7d32aa1
Austen Ito <austen.services@gmail.com>
2012-03-13 01:32:28 -0400

    Creating attendees and pretty printing

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,9 +1,28 @@
+$:.unshift File.dirname(__FILE__)
+require 'csv'
+require 'attendee'
+
 class Printer
   def print(attendees)
     puts "LAST NAME  FIRST NAME  EMAIL  ZIPCODE  CITY  STATE  ADDRESS"
+    attendees.each do |attendee|
+      puts attendee.last_name
+    end
   end
 
   def save_to(filename)
     
   end
 end
+
+p = Printer.new
+
+@file = CSV.open("../event_attendees.csv", {:headers => true, :header_converters => :symbol})
+attendees = []
+5.times do
+  line = @file.readline
+  record = line.to_hash
+  record.delete(:_)
+  attendees << Attendee.new(record)
+end
+p.print(attendees)
